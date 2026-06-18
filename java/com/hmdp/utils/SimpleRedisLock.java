f4092e95c44e8d35123d4a1aedd3167f2afbed6c
fcurry30 <543101743@qq.com>
2026-06-18 11:00:08 +0800

    秒杀券一人一单分布式锁基于LUA脚本的原子性业务功能代码

--- java/com/hmdp/utils/SimpleRedisLock.java
+++ java/com/hmdp/utils/SimpleRedisLock.java
@@ -3,9 +3,12 @@ package com.hmdp.utils;
 import cn.hutool.core.lang.UUID;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 @Data
@@ -14,6 +17,12 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private static final String key_prefix = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static{
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     private String name;
 
     @Override
@@ -23,13 +32,23 @@ public class SimpleRedisLock implements ILock{
                 .setIfAbsent(key_prefix + name, ID_PREFIX + id, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);//自动拆装箱时一定要注意空指针的风险。
     }
-
     @Override
-    public void unlock() {
-        String s = stringRedisTemplate.opsForValue().get(key_prefix + name);
-        String s1 = ID_PREFIX + Thread.currentThread().getId();
-        if(s1.equals(s)){
-            stringRedisTemplate.delete(key_prefix + name);
-        }
+    public void unlock() {  //调用lua脚本
+        //调用脚本
+        stringRedisTemplate.execute(
+          UNLOCK_SCRIPT
+          , Collections.singletonList(key_prefix + name)
+          , ID_PREFIX + Thread.currentThread().getId()
+        );
+
     }
+
+//    @Override
+//    public void unlock() {
+//        String s = stringRedisTemplate.opsForValue().get(key_prefix + name);
+//        String s1 = ID_PREFIX + Thread.currentThread().getId();
+//        if(s1.equals(s)){
+//            stringRedisTemplate.delete(key_prefix + name);
+//        }
+//    }
 }
